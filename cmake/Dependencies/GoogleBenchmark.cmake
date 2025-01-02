RobertAlbus/hwid_poc

initium magni operis

cmake/Dependencies/GoogleBenchmark.cmake
@@ -1,0 +1,40 @@
+set(HWID_POC_FETCH_GOOGLE_BENCHMARK_VERSION "1.9.0")
+set(HWID_POC_FETCH_GOOGLE_BENCHMARK_VERSION_TAG "v${HWID_POC_FETCH_GOOGLE_BENCHMARK_VERSION}")
+
+if(HWID_POC_FETCH_DEPENDENCIES AND HWID_POC_FETCH_GOOGLE_BENCHMARK)
+    include(FetchContent)
+
+    message("\n-- Google Benchmark (${HWID_POC_FETCH_GOOGLE_BENCHMARK_VERSION} via FetchContent)")
+
+    set(BENCHMARK_ENABLE_TESTING    OFF)
+    set(BENCHMARK_ENABLE_INSTALL    OFF)
+    set(BENCHMARK_ENABLE_EXCEPTIONS OFF)
+    set(BENCHMARK_INSTALL_DOCS      OFF)
+
+    FetchContent_Declare(
+        benchmark
+        GIT_REPOSITORY https://github.com/google/benchmark.git
+        GIT_TAG ${HWID_POC_FETCH_GOOGLE_BENCHMARK_VERSION_TAG}
+    )
+
+    set(old_CMAKE_MESSAGE_LOG_LEVEL ${CMAKE_MESSAGE_LOG_LEVEL})
+    set(CMAKE_MESSAGE_LOG_LEVEL WARNING)
+
+    FetchContent_MakeAvailable(benchmark)
+
+    # Disable clang-tidy for this dependency
+    set_target_properties(benchmark      PROPERTIES CXX_CLANG_TIDY "")
+    set_target_properties(benchmark_main PROPERTIES CXX_CLANG_TIDY "")
+
+    set(CMAKE_MESSAGE_LOG_LEVEL ${old_CMAKE_MESSAGE_LOG_LEVEL})
+
+else()
+    message("\n-- Google Benchmark (${HWID_POC_FETCH_GOOGLE_BENCHMARK_VERSION} via find_package)")
+
+    set(old_CMAKE_MESSAGE_LOG_LEVEL ${CMAKE_MESSAGE_LOG_LEVEL})
+    set(CMAKE_MESSAGE_LOG_LEVEL WARNING)
+
+    find_package(benchmark ${HWID_POC_FETCH_GOOGLE_BENCHMARK_VERSION} REQUIRED CONFIG)
+
+    set(CMAKE_MESSAGE_LOG_LEVEL ${old_CMAKE_MESSAGE_LOG_LEVEL})
+endif()

cmake/Dependencies/GoogleTest.cmake
@@ -1,0 +1,38 @@
+set(HWID_POC_FETCH_GOOGLE_TEST_VERSION "1.15.2")
+set(HWID_POC_FETCH_GOOGLE_TEST_VERSION_TAG "v${HWID_POC_FETCH_GOOGLE_TEST_VERSION}")
+
+if(HWID_POC_FETCH_DEPENDENCIES AND HWID_POC_FETCH_GOOGLE_TEST)
+    include(FetchContent)
+
+    message("\n-- Google Test (${HWID_POC_FETCH_GOOGLE_TEST_VERSION} via FetchContent)")
+
+    set(BUILD_GMOCK   OFF)
+    set(INSTALL_GTEST OFF)
+
+    FetchContent_Declare(
+        googletest
+        GIT_REPOSITORY https://github.com/google/googletest.git
+        GIT_TAG ${HWID_POC_FETCH_GOOGLE_TEST_VERSION_TAG}
+    )
+
+    set(old_CMAKE_MESSAGE_LOG_LEVEL ${CMAKE_MESSAGE_LOG_LEVEL})
+    set(CMAKE_MESSAGE_LOG_LEVEL WARNING)
+
+    FetchContent_MakeAvailable(googletest)
+
+    # Disable clang-tidy for this dependency
+    set_target_properties(gtest         PROPERTIES CXX_CLANG_TIDY "")
+    set_target_properties(gtest_main    PROPERTIES CXX_CLANG_TIDY "")
+
+    set(CMAKE_MESSAGE_LOG_LEVEL ${old_CMAKE_MESSAGE_LOG_LEVEL})
+
+else()
+    message("\n-- Google Test (${HWID_POC_FETCH_GOOGLE_TEST_VERSION} via find_package)")
+
+    set(old_CMAKE_MESSAGE_LOG_LEVEL ${CMAKE_MESSAGE_LOG_LEVEL})
+    set(CMAKE_MESSAGE_LOG_LEVEL WARNING)
+
+    find_package(GTest "${HWID_POC_FETCH_GOOGLE_TEST_VERSION}" REQUIRED CONFIG)
+
+    set(CMAKE_MESSAGE_LOG_LEVEL ${old_CMAKE_MESSAGE_LOG_LEVEL})
+endif()

src/hwid_poc_benchmark/benchmark_main.cpp
@@ -1,0 +1,14 @@
+#include "benchmark/benchmark.h"
+
+#include "hwid_poc/example.hpp"
+
+static void BM_example(benchmark::State& state) {
+    long n = state.range(0);
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(Hwid_poc::example());
+    }
+}
+
+BENCHMARK(BM_example)->Arg(10)->Arg(15)->Arg(20);
+
+BENCHMARK_MAIN();

src/hwid_poc_lib/hwid_poc/example.hpp
@@ -1,0 +1,7 @@
+#pragma once
+
+namespace Hwid_poc {
+
+auto example() -> int;
+
+};

src/hwid_poc_lib/impl/example.cpp
@@ -1,0 +1,3 @@
+#include "hwid_poc/example.hpp"
+
+auto Hwid_poc::example() -> int { return 0; }

RobertAlbus/clover_sketches

cmake/Dependencies/GoogleBenchmark.cmake
@@ -1,0 +1,40 @@
+set(CLOVER_SKETCHES_FETCH_GOOGLE_BENCHMARK_VERSION "1.9.0")
+set(CLOVER_SKETCHES_FETCH_GOOGLE_BENCHMARK_VERSION_TAG "v${CLOVER_SKETCHES_FETCH_GOOGLE_BENCHMARK_VERSION}")
+
+if(CLOVER_SKETCHES_FETCH_DEPENDENCIES AND CLOVER_SKETCHES_FETCH_GOOGLE_BENCHMARK)
+    include(FetchContent)
+
+    message("\n-- Google Benchmark (${CLOVER_SKETCHES_FETCH_GOOGLE_BENCHMARK_VERSION} via FetchContent)")
+
+    set(BENCHMARK_ENABLE_TESTING    OFF)
+    set(BENCHMARK_ENABLE_INSTALL    OFF)
+    set(BENCHMARK_ENABLE_EXCEPTIONS OFF)
+    set(BENCHMARK_INSTALL_DOCS      OFF)
+
+    FetchContent_Declare(
+        benchmark
+        GIT_REPOSITORY https://github.com/google/benchmark.git
+        GIT_TAG ${CLOVER_SKETCHES_FETCH_GOOGLE_BENCHMARK_VERSION_TAG}
+    )
+
+    set(old_CMAKE_MESSAGE_LOG_LEVEL ${CMAKE_MESSAGE_LOG_LEVEL})
+    set(CMAKE_MESSAGE_LOG_LEVEL WARNING)
+
+    FetchContent_MakeAvailable(benchmark)
+
+    # Disable clang-tidy for this dependency
+    set_target_properties(benchmark      PROPERTIES CXX_CLANG_TIDY "")
+    set_target_properties(benchmark_main PROPERTIES CXX_CLANG_TIDY "")
+
+    set(CMAKE_MESSAGE_LOG_LEVEL ${old_CMAKE_MESSAGE_LOG_LEVEL})
+
+else()
+    message("\n-- Google Benchmark (${CLOVER_SKETCHES_FETCH_GOOGLE_BENCHMARK_VERSION} via find_package)")
+
+    set(old_CMAKE_MESSAGE_LOG_LEVEL ${CMAKE_MESSAGE_LOG_LEVEL})
+    set(CMAKE_MESSAGE_LOG_LEVEL WARNING)
+
+    find_package(benchmark ${CLOVER_SKETCHES_FETCH_GOOGLE_BENCHMARK_VERSION} REQUIRED CONFIG)
+
+    set(CMAKE_MESSAGE_LOG_LEVEL ${old_CMAKE_MESSAGE_LOG_LEVEL})
+endif()

cmake/Dependencies/GoogleTest.cmake
@@ -1,0 +1,38 @@
+set(CLOVER_SKETCHES_FETCH_GOOGLE_TEST_VERSION "1.15.2")
+set(CLOVER_SKETCHES_FETCH_GOOGLE_TEST_VERSION_TAG "v${CLOVER_SKETCHES_FETCH_GOOGLE_TEST_VERSION}")
+
+if(CLOVER_SKETCHES_FETCH_DEPENDENCIES AND CLOVER_SKETCHES_FETCH_GOOGLE_TEST)
+    include(FetchContent)
+
+    message("\n-- Google Test (${CLOVER_SKETCHES_FETCH_GOOGLE_TEST_VERSION} via FetchContent)")
+
+    set(BUILD_GMOCK   OFF)
+    set(INSTALL_GTEST OFF)
+
+    FetchContent_Declare(
+        googletest
+        GIT_REPOSITORY https://github.com/google/googletest.git
+        GIT_TAG ${CLOVER_SKETCHES_FETCH_GOOGLE_TEST_VERSION_TAG}
+    )
+
+    set(old_CMAKE_MESSAGE_LOG_LEVEL ${CMAKE_MESSAGE_LOG_LEVEL})
+    set(CMAKE_MESSAGE_LOG_LEVEL WARNING)
+
+    FetchContent_MakeAvailable(googletest)
+
+    # Disable clang-tidy for this dependency
+    set_target_properties(gtest         PROPERTIES CXX_CLANG_TIDY "")
+    set_target_properties(gtest_main    PROPERTIES CXX_CLANG_TIDY "")
+
+    set(CMAKE_MESSAGE_LOG_LEVEL ${old_CMAKE_MESSAGE_LOG_LEVEL})
+
+else()
+    message("\n-- Google Test (${CLOVER_SKETCHES_FETCH_GOOGLE_TEST_VERSION} via find_package)")
+
+    set(old_CMAKE_MESSAGE_LOG_LEVEL ${CMAKE_MESSAGE_LOG_LEVEL})
+    set(CMAKE_MESSAGE_LOG_LEVEL WARNING)
+
+    find_package(GTest "${CLOVER_SKETCHES_FETCH_GOOGLE_TEST_VERSION}" REQUIRED CONFIG)
+
+    set(CMAKE_MESSAGE_LOG_LEVEL ${old_CMAKE_MESSAGE_LOG_LEVEL})
+endif()

src/clover_sketches_benchmark/benchmark_main.cpp
@@ -1,0 +1,14 @@
+#include "benchmark/benchmark.h"
+
+#include "clover_sketches/example.hpp"
+
+static void BM_example(benchmark::State& state) {
+    long n = state.range(0);
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(clover_sketches::example());
+    }
+}
+
+BENCHMARK(BM_example)->Arg(10)->Arg(15)->Arg(20);
+
+BENCHMARK_MAIN();

src/clover_sketches_lib/clover_sketches/example.hpp
@@ -1,0 +1,7 @@
+#pragma once
+
+namespace clover_sketches {
+
+auto example() -> int;
+
+};

src/clover_sketches_lib/impl/example.cpp
@@ -1,0 +1,3 @@
+#include "clover_sketches/example.hpp"
+
+auto clover_sketches::example() -> int { return 0; }