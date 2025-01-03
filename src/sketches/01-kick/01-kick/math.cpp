9e1a856085b46d72bdab34f8a6c1a4290c2a89b9
Rob Albus <hello@robertalbus.com>
2025-01-02 17:50:56 -0700

    create kick drum sketch

---
+++ src/sketches/01-kick/01-kick/math.cpp
@@ -0,0 +1,19 @@
+// Sketches with Clover Audio Framework
+// Copyright (C) 2025  Rob W. Albus
+// Licensed under the GPLv3. See LICENSE for details.
+
+#include <cmath>
+
+#include "clover/float.hpp"
+
+#include "math.hpp"
+
+namespace sketch {
+auto octave_difference_by_frequency(clover_float base_freq, clover_float target_freq) -> clover_float {
+    return std::log2(target_freq / base_freq);
+}
+
+auto semitone_difference_by_frequency(clover_float base_freq, clover_float target_freq) -> clover_float {
+    return octave_difference_by_frequency(base_freq, target_freq) * 12.f;
+}
+}  // namespace sketch
--- src/sketches/01-kick/main.cpp
+++ src/sketches/01-kick/main.cpp
@@ -2,12 +2,18 @@
 // Copyright (C) 2025  Rob W. Albus
 // Licensed under the GPLv3. See LICENSE for details.
 
+#include <cmath>
+
+#include "clover/dsp/env_adsr.hpp"
+#include "clover/dsp/filter.hpp"
 #include "clover/dsp/oscillator.hpp"
 #include "clover/io/audio_callback.hpp"
 #include "clover/io/audio_file.hpp"
 #include "clover/io/stream.hpp"
 #include "clover/io/system_audio.hpp"
 #include "clover/math.hpp"
+
+#include "01-kick/math.hpp"
 
 bool PLAYBACK = true;
 bool RENDER   = false;
@@ -21,28 +27,78 @@ using namespace io;
 int main(int argc, char *argv[]) {
     float fs              = 48000;
     int fs_i              = static_cast<int>(fs);
-    int duration          = 5 * fs_i;
+    int duration          = 200 * fs_i;
     int channel_count_out = 2;
 
-    oscillator osc(fs);
-    oscillator mod(fs);
+    oscillator kick_osc(fs);
+    env_adsr kick_adsr_gain;
+    env_adsr kick_adsr_pitch;
+
+    clover_float kick_osc_pitch_fundamental = 50;
+    clover_float kick_osc_pitch_peak        = 200;
+    clover_float kick_osc_pitch_range =
+            sketch::octave_difference_by_frequency(kick_osc_pitch_fundamental, kick_osc_pitch_peak);
+
+    filter filt;
+    env_adsr kick_adsr_cutoff;
+    clover_float kick_osc_cutoff_fundamental = 100;
+    clover_float kick_osc_cutoff_peak        = 200;
+    clover_float kick_osc_cutoff_range =
+            sketch::octave_difference_by_frequency(kick_osc_pitch_fundamental, kick_osc_pitch_peak);
 
     auto audio_state_init = [&]() {
-        osc.freq(400);
-        mod.freq(2);
-        osc.phase(0);
-        mod.phase(0);
+        kick_osc.freq(kick_osc_pitch_fundamental);
+        kick_osc.phase(0);
+
+        kick_adsr_cutoff.set(1, 2000, 0, 2000);
+        kick_adsr_pitch.set(1, 3000, 0, 1);
+        kick_adsr_gain.set(1, 5000, 0.1, 5000);
+
+        filt.m_coeffs = lpf(fs, kick_osc_cutoff_fundamental, 1);
     };
 
-    auto audio_callback = [&](callback_args data) {
+    int_fast64_t counter = 0;
+    auto audio_callback  = [&](callback_args data) {
         float &L = *(data.output);
         float &R = *(data.output + 1);
 
-        L = osc.tick();
-        R = L;
+        auto time = static_cast<clover_float>(data.clock_time);
 
-        float freq = frequency_by_octave_difference(400, (7.f / 12.f) * mod.tick());
-        osc.freq(freq);
+        if (counter == 0) {
+            kick_osc.phase(0);
+            kick_adsr_cutoff.key_on();
+            kick_adsr_pitch.key_on();
+            kick_adsr_gain.key_on();
+
+        } else if (counter == 10000) {
+            kick_adsr_cutoff.key_off();
+            kick_adsr_pitch.key_off();
+            kick_adsr_gain.key_off();
+        }
+
+        if (++counter == 24000) {
+            counter = 0;
+        }
+
+        clover_float kick_osc_signal = kick_osc.tick();
+        clover_float kick_cutoff_env = kick_adsr_cutoff.tick();
+        clover_float kick_gain_env   = kick_adsr_gain.tick();
+        clover_float kick_pitch_env  = kick_adsr_pitch.tick();
+
+        clover_float kick_signal = kick_osc_signal * kick_gain_env;
+        kick_signal              = std::tanh(kick_signal * 1.4f);
+        kick_signal              = filt.tick(kick_signal);
+
+        float kick_pitch = frequency_by_octave_difference(
+                kick_osc_pitch_fundamental, kick_osc_pitch_range * kick_pitch_env);
+        kick_osc.freq(kick_pitch);
+
+        float kick_cutoff = frequency_by_octave_difference(
+                kick_osc_cutoff_fundamental, kick_osc_cutoff_range * kick_cutoff_env);
+        filt.m_coeffs = lpf(fs, kick_cutoff, 1);
+
+        L = kick_signal;
+        R = L;
 
         if (data.clock_time == duration) {
             return callback_status::end;
