7b3919486fb14b4c7130e9a39d65096b08a0a68a
Rob Albus <hello@robertalbus.com>
2025-02-08 13:08:45 -0700

    012_preset_fdn add FDN algorithm from 010_fdn_better

---
+++ src/sketches/012_preset_fdn/fdn/hadamard.hpp
@@ -0,0 +1,63 @@
+#pragma once
+
+// Sketches with Clover Audio Framework
+// Copyright (C) 2025  Rob W. Albus
+// Licensed under the GPLv3. See LICENSE for details.
+
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <format>
+#include <ranges>
+#include <stdexcept>
+
+extern const std::array<float, 4> hadamard_2x2;
+extern const std::array<float, 16> hadamard_4x4;
+extern const std::array<float, 64> hadamard_8x8;
+extern const std::array<float, 256> hadamard_16x16;
+
+constexpr float scale(const float x) noexcept {
+    // C++23 constexpr std::sqrt
+    return 1 / std::sqrt(x);
+}
+
+template <int N>
+const std::array<float, N * N>& get_matrix() {
+    if constexpr (N == 2) {
+        return hadamard_2x2;
+    } else if constexpr (N == 4) {
+        return hadamard_4x4;
+    } else if constexpr (N == 8) {
+        return hadamard_8x8;
+    } else if constexpr (N == 16) {
+        return hadamard_16x16;
+    } else {
+        throw std::invalid_argument(std::format("get_matrix<N>() for N=%d", N));
+    };
+}
+
+template <int N>
+std::array<float, N> process_hadamard(std::array<float, N>& inputs) {
+    std::array<float, N> inverses;
+    auto inverted_view = std::views::all(inputs) | std::views::transform([](float x) { return x * -1.f; });
+    std::ranges::copy(inverted_view, inverses.begin());
+
+    auto columns = std::views::iota(0, N) | std::views::transform([](size_t i) {
+                       return get_matrix<N>() | std::views::drop(i) | std::views::stride(N);
+                   });
+
+    std::array<float, N> outputs;
+    outputs.fill(0);
+
+    for (auto [output, column] : std::views::zip(outputs, columns)) {
+        for (auto [input, inverse, element] : std::views::zip(inputs, inverses, column)) {
+            float result = element == 1 ? input : inverse;
+            output += result;
+        }
+    }
+
+    for (auto& value : outputs)
+        value *= scale(N);
+
+    return outputs;
+}
