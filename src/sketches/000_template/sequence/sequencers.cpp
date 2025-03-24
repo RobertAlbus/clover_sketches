616e894a9edd5aadba85dcc5054ceb32f8c5abce
Robert Albus <hello@robertalbus.com>
2025-03-24 08:15:44 -0600

    000_template refactor the template to reflect the latest approach

---
+++ src/sketches/000_template/sequence/sequencers.cpp
@@ -0,0 +1,12 @@
+// Sketches with Clover Audio Framework
+// Copyright (C) 2025  Rob W. Albus
+// Licensed under the GPLv3. See LICENSE for details.
+
+#include "sequencers.hpp"
+#include "composition/composition.hpp"
+
+sequencers::sequencers(composition& comp) {
+}
+
+void sequencers::tick() {
+}
