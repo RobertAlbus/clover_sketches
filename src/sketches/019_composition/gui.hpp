0fbb1f7142c54736d49cc7a3d9544f8be2b8b55f
Rob Albus <hello@robertalbus.com>
2025-03-25 18:44:33 -0600

    019_composition scaffold from 000_template

---
+++ src/sketches/019_composition/gui.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+// Sketches with Clover Audio Framework
+// Copyright (C) 2025  Rob W. Albus
+// Licensed under the GPLv3. See LICENSE for details.
+
+#include "hello_imgui/hello_imgui.h"
+#include "imgui.h"
+#include <print>
+using namespace ImGui;
+
+#include "shared_props.hpp"
+
+void GUI(shared_props& props) {
+    props.audio_ready.acquire();
+    // gui setup before audio starts
+
+    auto guiFunction = [&]() {
+        ImGui::NewLine();
+        if (ImGui::Button("Bye!")) {
+            HelloImGui::GetRunnerParams()->appShallExit = true;
+        }
+        ImGui::Text("Framerate: %.2f", ImGui::GetIO().Framerate);
+    };
+
+    HelloImGui::RunnerParams params{};
+
+    params.callbacks.EnqueueBeforeExit([&]() { props.gui_intent_to_exit.release(); });
+
+    params.callbacks.ShowGui           = guiFunction;
+    params.appWindowParams.windowTitle = "Hello, Audio";
+    params.fpsIdling.enableIdling      = true;
+    HelloImGui::Run(params);
+}
