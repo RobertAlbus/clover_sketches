512416682cb57de6c7ee808a73faed05ddb51684
Robert Albus <hello@robertalbus.com>
2025-03-28 22:34:37 -0600

    019_composition fix audio callback creation so that rendering and playback don't use the same instance of the callback and composition

--- src/sketches/019_composition/audio.hpp
+++ src/sketches/019_composition/audio.hpp
@@ -22,11 +22,8 @@
 
 std::string render_name{"019_composition"};
 
-void AUDIO(shared_props &props) {
-    composition comp;
-    sequencers sqs{comp};
-
-    auto audio_callback = [&](clover::io::callback_args data) {
+auto create_audio_callback(composition &comp, sequencers &sqs) {
+    return [&](clover::io::callback_args data) {
         float &L = *(data.output);
         float &R = *(data.output + 1);
 
@@ -38,23 +35,34 @@ void AUDIO(shared_props &props) {
         }
         return clover::io::callback_status::cont;
     };
+}
 
-    std::jthread render_thread = std::jthread([&]() {
+void AUDIO(shared_props &props) {
+    composition comp;
+    sequencers sqs{comp};
+
+    auto audio_callback = create_audio_callback(comp, sqs);
+
+    std::jthread render_thread = std::jthread([]() {
         std::cout << "starting render: " << render_name.c_str() << std::endl;
-        composition comp;
+
+        composition render_comp;
+        sequencers render_sqs{render_comp};
+
+        auto audio_callback = create_audio_callback(render_comp, render_sqs);
 
         audio_buffer buffer;
-        buffer.channels    = comp.channel_count_out;
-        buffer.sample_rate = comp.fs_i;
-        buffer.data.resize(comp.duration * comp.channel_count_out, 0.f);
+        buffer.channels    = render_comp.channel_count_out;
+        buffer.sample_rate = render_comp.fs_i;
+        buffer.data.resize(render_comp.duration * render_comp.channel_count_out, 0.f);
 
-        for (auto frame : std::views::iota(0, comp.duration)) {
+        for (auto frame : std::views::iota(0, render_comp.duration)) {
             auto result = audio_callback({
                     .clock_time     = frame,
                     .chan_count_in  = 0,
-                    .chan_count_out = comp.channel_count_out,
+                    .chan_count_out = render_comp.channel_count_out,
                     .input          = nullptr,
-                    .output         = &(buffer.data[static_cast<size_t>(frame) * comp.channel_count_out]),
+                    .output = &(buffer.data[static_cast<size_t>(frame) * render_comp.channel_count_out]),
             });
         }
 
