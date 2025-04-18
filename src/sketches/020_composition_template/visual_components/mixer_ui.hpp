806ea55e4507fd1d86c931ad5002a3e3ef513517
Robert Albus <hello@robertalbus.com>
2025-04-18 07:05:01 -0600

    020_composition_template delete composition template because architectural progress is being made in 021_sd_template

--- src/sketches/020_composition_template/visual_components/mixer_ui.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// Sketches with Clover Audio Framework
-// Copyright (C) 2025  Rob W. Albus
-// Licensed under the GPLv3. See LICENSE for details.
-
-#include "composition/mix.hpp"
-
-void mixer_component(const char* id, composition_mix* mixer);
