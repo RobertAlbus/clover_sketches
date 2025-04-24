06159b0d67bf71acc78b6397c47e99da18c9ecf8
Robert Albus <hello@robertalbus.com>
2025-04-23 20:00:57 -0600

    021_sd_template remove indirection to mixer values because it's not working in the gui

--- src/sketches/021_sd_template/composition/graph.cpp
+++ src/sketches/021_sd_template/composition/graph.cpp
@@ -20,32 +20,24 @@ std::pair<float, float> signal_graph::tick() {
     float out_L = 0;
     float out_R = 0;
 
-    static float& MIX_kick_dry  = audio_mixer.at("kick dry");
-    static float& MIX_kick_send = audio_mixer.at("kick send");
-    static float& MIX_kick_wet  = audio_mixer.at("kick wet");
-    static float& MIX_kick_bus  = audio_mixer.at("kick bus");
-
-    static float& MIX_chord_dry  = audio_mixer.at("chord dry");
-    static float& MIX_chord_send = audio_mixer.at("chord send");
-    static float& MIX_chord_wet  = audio_mixer.at("chord wet");
-    static float& MIX_chord_bus  = audio_mixer.at("chord bus");
-
     // ----------------
     // FOUNDATION
     //
     //
 
     float kick_dry  = kick.tick();
-    float kick_send = kick_dry * MIX_kick_send;
-    kick_dry *= MIX_kick_dry;
+    float kick_send = kick_dry * audio_mixer.at("kick send");
+    ;
+    kick_dry *= audio_mixer.at("kick dry");
+    ;
     auto [kick_send_eq, _] = kick_preverb_peq.tick(kick_send, kick_send);
 
-    float kick_wet = kick_verb.tick(kick_send_eq) * MIX_kick_wet;  // * kick_auto_verb_send.tick();
-    float kick_sum = (kick_dry + kick_wet) * MIX_kick_bus;
+    float kick_wet = kick_verb.tick(kick_send_eq) * audio_mixer.at("kick wet") * kick_auto_verb_send.tick();
+    float kick_sum = (kick_dry + kick_wet) * audio_mixer.at("kick bus");
 
-    // kick_hpf.m_coeffs = hpf(grid.fs, frequency_by_octave_difference(10, kick_auto_hp.tick()), 0.707);
-    // kick_hpf.m_coeffs = hpf(grid.fs, frequency_by_octave_difference(10, 0), 0.707);
-    kick_sum = kick_sum;  // kick_hpf.tick(kick_sum);
+    kick_hpf.m_coeffs = hpf(grid.fs, frequency_by_octave_difference(10, kick_auto_hp.tick()), 0.707);
+    kick_hpf.m_coeffs = hpf(grid.fs, frequency_by_octave_difference(10, 0), 0.707);
+    kick_sum          = kick_hpf.tick(kick_sum);
 
     auto [kick_post_eq, _] = kick_out_peq.tick(kick_sum, kick_sum);
     kick_sum               = kick_post_eq;
@@ -62,28 +54,28 @@ std::pair<float, float> signal_graph::tick() {
         chord_L += chord_voice_signal.first;
         chord_R += chord_voice_signal.second;
     }
-    float chord_send_L                      = chord_L * MIX_chord_send;
-    float chord_send_R                      = chord_R * MIX_chord_send;
+    float chord_send_L                      = chord_L * audio_mixer.at("chord send");
+    float chord_send_R                      = chord_R * audio_mixer.at("chord send");
     auto [chord_preverb_L, chord_preverb_R] = chord_preverb_peq.tick(chord_send_L, chord_send_R);
     chord_preverb_L                         = std::clamp(chord_preverb_L, -1.f, 1.f);
     chord_preverb_R                         = std::clamp(chord_preverb_R, -1.f, 1.f);
 
-    float chord_verb_out_L = chord_verb_L.tick(chord_preverb_L) * MIX_chord_wet;
-    float chord_verb_out_R = chord_verb_R.tick(chord_preverb_R) * MIX_chord_wet;
+    float chord_verb_out_L = chord_verb_L.tick(chord_preverb_L) * audio_mixer.at("chord wet");
+    float chord_verb_out_R = chord_verb_R.tick(chord_preverb_R) * audio_mixer.at("chord wet");
 
     chord_verb_out_L = std::clamp(chord_verb_out_L, -1.f, 1.f);
     chord_verb_out_R = std::clamp(chord_verb_out_R, -1.f, 1.f);
 
-    chord_L *= MIX_chord_dry;
-    chord_R *= MIX_chord_dry;
+    chord_L *= audio_mixer.at("chord dry");
+    chord_R *= audio_mixer.at("chord dry");
 
     float chord_post_eq_in_L = std::clamp((chord_L + chord_verb_out_L), -1.f, 1.f);
     float chord_post_eq_in_R = std::clamp((chord_R + chord_verb_out_R), -1.f, 1.f);
 
     auto [chord_post_eq_L, chord_post_eq_R] = chord_peq.tick(chord_post_eq_in_L, chord_post_eq_in_R);
 
-    float chord_sum_L = chord_post_eq_L * MIX_chord_bus;
-    float chord_sum_R = chord_post_eq_R * MIX_chord_bus;
+    float chord_sum_L = chord_post_eq_L * audio_mixer.at("chord bus");
+    float chord_sum_R = chord_post_eq_R * audio_mixer.at("chord bus");
 
     // ----------------
     // SUMMING
