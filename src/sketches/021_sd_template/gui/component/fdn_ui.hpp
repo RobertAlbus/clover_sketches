434b3daa3b23c7a645cc86c46b27ee3960ec13f7
Robert Albus <hello@robertalbus.com>
2025-05-04 09:41:38 -0600

    delete unused template sketches

--- src/sketches/021_sd_template/gui/component/fdn_ui.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// Sketches with Clover Audio Framework
-// Copyright (C) 2025  Rob W. Albus
-// Licensed under the GPLv3. See LICENSE for details.
-
-#include "instruments/fdn.hpp"
-
-void fdn_component(const char* id, fdn_8_019* fdn_L, fdn_8_019* fdn_R);
--- src/sketches/021_sd_template/gui/component/peq.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// Sketches with Clover Audio Framework
-// Copyright (C) 2025  Rob W. Albus
-// Licensed under the GPLv3. See LICENSE for details.
-
-#include "instruments/peq.hpp"
-
-void peq_gui(const char* id, peq& peq);
