7b30b1b6b37e4ae7198f4c8b7929246e5508ecba
Robert Albus <hello@robertalbus.com>
2025-05-11 18:07:53 -0600

    023_fdn tweak the patch

--- src/sketches/023_fdn/graph/patches/patch_drums.cpp
+++ src/sketches/023_fdn/graph/patches/patch_drums.cpp
@@ -11,11 +11,11 @@ patch_drums_t::patch_drums_t() {
     // --------------------------------
     // KICK
     kick_drum_props = {
-            .trim              = 0.512,
-            .drive             = 8.638,
+            .trim              = 0.631,
+            .drive             = 2.738,
             .amp_a             = 30,
-            .amp_d             = 5000,
-            .amp_s             = 0.25475892,
+            .amp_d             = 12142.683,
+            .amp_s             = 0,
             .amp_r             = 1000,
             .cut_a             = 1,
             .cut_d             = 3905.1545,
@@ -23,19 +23,19 @@ patch_drums_t::patch_drums_t() {
             .cut_r             = 1.0000596,
             .filt_q            = 1.3999405,
             .pitch_a           = 1,
-            .pitch_d           = 4071.2004,
+            .pitch_d           = 1690.4856,
             .pitch_s           = 0,
             .pitch_r           = 1000,
-            .pitch_fundamental = midi_to_frequency(note::Fs2) / 2,
+            .pitch_fundamental = midi_to_frequency(note::F2) / 2,
             .cut_fundamental   = 100,
-            .pitch_range       = 2,
+            .pitch_range       = 1.17,
             .cut_range         = 3,
     };
 
     kick_fdn_props = {
-            .taps    = {134.42862, 329.99323, 721.1825, 1308.0359, 1959.9309, 3329.2222, 6458.37, 11087.26},
+            .taps    = {134.42862, 1223.932, 1562.802, 2172.768, 2918.282, 3329.2222, 4612.633, 6645.853},
             .fb_gain = 0.87402976,
-            .lpf_cut = 180.31549,
+            .lpf_cut = 373.7427,
             .lpf_res = 0.707,
             .hpf_cut = 45.100502,
             .hpf_res = 0.707,
