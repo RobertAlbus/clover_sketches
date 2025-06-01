f73908db63e177203ab9c9bdcd1f9fba915c3cf8
Rob Albus <hello@robertalbus.com>
2025-06-01 17:06:15 -0600

    019_composition remove hello_imgui

--- cmake/dependencies/gui.cmake
+++ cmake/dependencies/gui.cmake
@@ -16,39 +16,16 @@ if(CLOVER_SKETCHES_FETCH_DEPENDENCIES)
         GIT_TAG        "3.3.4"
     )
 
-    set(HELLOIMGUI_IMGUI_SOURCE_DIR ${imgui_SOURCE_DIR} CACHE PATH "Path to Dear ImGui")
-
-    set(HELLOIMGUI_ADD_APP_WITH_INSTALL OFF CACHE BOOL "HELLOIMGUI_ADD_APP_WITH_INSTALL" FORCE)
-    set(HELLOIMGUI_BUILD_IMGUI          OFF CACHE BOOL "HELLOIMGUI_BUILD_IMGUI"          FORCE)
-    set(HELLOIMGUI_HAS_OPENGL3          ON  CACHE BOOL "HELLOIMGUI_HAS_OPENGL3"          FORCE)
-    set(HELLOIMGUI_USE_GLFW3            ON  CACHE BOOL "HELLOIMGUI_USE_GLFW3"            FORCE)
-    set(HELLOIMGUI_INSTALL              OFF CACHE BOOL "HELLOIMGUI_INSTALL"              FORCE)
-    FetchContent_Declare(
-        hello_imgui
-        GIT_SHALLOW TRUE
-        GIT_REPOSITORY https://github.com/pthom/hello_imgui.git
-        GIT_TAG        "v1.6.3"
-    )
-
     # --------------------------------
     # make available
 
     set(OLD_CMAKE_MESSAGE_LOG_LEVEL ${CMAKE_MESSAGE_LOG_LEVEL})
     set(CMAKE_MESSAGE_LOG_LEVEL WARNING)
-
+    
     FetchContent_MakeAvailable(glfw3)
     set(glfw3_DIR ${glfw3_BINARY_DIR}/src)
     set_target_properties(glfw PROPERTIES CXX_CLANG_TIDY "")
-
-    FetchContent_MakeAvailable(hello_imgui)
-    target_include_directories(hello_imgui PUBLIC
-        ${hello_imgui_SOURCE_DIR}/src
-    )
-
-    set_target_properties(imgui PROPERTIES CXX_CLANG_TIDY "")
-    set_target_properties(hello_imgui PROPERTIES CXX_CLANG_TIDY "")
-    set_target_properties(stb_hello_imgui PROPERTIES CXX_CLANG_TIDY "")
-        
+    
     set(CMAKE_MESSAGE_LOG_LEVEL ${OLD_CMAKE_MESSAGE_LOG_LEVEL})
 
 
@@ -86,19 +63,19 @@ if(CLOVER_SKETCHES_FETCH_DEPENDENCIES)
     file(GLOB IMGUI_KNOBS_SOURCES ${imgui_knobs_SOURCE_DIR}/*.cpp)
     add_library(imgui_knobs_lib STATIC ${IMGUI_KNOBS_SOURCES})
     target_include_directories( imgui_knobs_lib PUBLIC ${imgui_knobs_SOURCE_DIR})
-    target_link_libraries(      imgui_knobs_lib PRIVATE hello_imgui             )
+    target_link_libraries(      imgui_knobs_lib PRIVATE imgui                   )
     set_target_properties(      imgui_knobs_lib PROPERTIES CXX_CLANG_TIDY ""    )
     
     file(GLOB IMPLOT_SOURCES ${implot_SOURCE_DIR}/*.cpp)
     add_library(implot_lib STATIC ${IMPLOT_SOURCES})
     target_include_directories( implot_lib PUBLIC ${implot_SOURCE_DIR} )
-    target_link_libraries(      implot_lib PRIVATE hello_imgui         )
+    target_link_libraries(      implot_lib PRIVATE imgui               )
     set_target_properties(      implot_lib PROPERTIES CXX_CLANG_TIDY "")
     
     file(GLOB IMPLOT3D_SOURCES ${implot3d_SOURCE_DIR}/*.cpp)
     add_library(implot3d_lib STATIC ${IMPLOT3D_SOURCES})
     target_include_directories( implot3d_lib PUBLIC ${implot3d_SOURCE_DIR})
-    target_link_libraries(      implot3d_lib PRIVATE hello_imgui          )
+    target_link_libraries(      implot3d_lib PRIVATE imgui                )
     set_target_properties(      implot3d_lib PROPERTIES CXX_CLANG_TIDY "" )
     
     add_library(sketch_gui_libs INTERFACE)
---
+++ src/sketches/019_composition/gui.cpp
@@ -0,0 +1,225 @@
+// Sketches with Clover Audio Framework
+// Copyright (C) 2025  Rob W. Albus
+// Licensed under the GPLv3. See LICENSE for details.
+
+// Dear ImGui: standalone example application for GLFW + OpenGL 3, using programmable pipeline
+// (GLFW is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan/Metal
+// graphics context creation, etc.)
+
+// Learn about Dear ImGui:
+// - FAQ                  https://dearimgui.com/faq
+// - Getting Started      https://dearimgui.com/getting-started
+// - Documentation        https://dearimgui.com/docs (same as your local docs/ folder).
+// - Introduction, links and more at the top of imgui.cpp
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include "implot.h"
+#define GL_SILENCE_DEPRECATION
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <GLES2/gl2.h>
+#endif
+#include <GLFW/glfw3.h>  // Will drag system OpenGL headers
+
+using namespace ImGui;
+
+#include "gui.hpp"
+#include "shared_props.hpp"
+#include "views/views.hpp"
+
+auto guiFunction = [](context& ctx) {
+    ImGuiViewport* viewport = ImGui::GetMainViewport();
+    ImGui::SetNextWindowPos(viewport->WorkPos);
+    ImGui::SetNextWindowSize(viewport->WorkSize);
+    ImGui::SetNextWindowViewport(viewport->ID);
+
+    ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse |
+                                    ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove |
+                                    ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus |
+                                    ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_MenuBar;
+
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+
+    ImGui::Begin("MainDockSpaceHost", nullptr, window_flags);
+    ImGui::PopStyleVar(2);
+
+    ImGuiID dockspace_id = ImGui::GetID("MainDockSpace");
+    ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), ImGuiDockNodeFlags_None);
+
+    if (ImGui::BeginTabBar("Main Layout Tabs")) {
+        if (ImGui::BeginTabItem("mixer")) {
+            controller_mixer("##kick_drum_gui", ctx.composition);
+
+            ImGui::EndTabItem();
+        }
+        if (ImGui::BeginTabItem("kick")) {
+            controller_kick("kick", ctx.composition);
+            ImGui::EndTabItem();
+        }
+        if (ImGui::BeginTabItem("bass")) {
+            view_bass("bass", ctx.composition);
+            ImGui::EndTabItem();
+        }
+        if (ImGui::BeginTabItem("cymbal")) {
+            view_cymbal("cymbal", ctx.composition);
+            ImGui::EndTabItem();
+        }
+        if (ImGui::BeginTabItem("lead_a")) {
+            view_lead_a("lead_a", ctx.composition);
+            ImGui::EndTabItem();
+        }
+        if (ImGui::BeginTabItem("lead_b")) {
+            view_lead_b("lead_b", ctx.composition);
+            ImGui::EndTabItem();
+        }
+        if (ImGui::BeginTabItem("chord")) {
+            controller_chord("chord", ctx.composition);
+            ImGui::EndTabItem();
+        }
+        if (ImGui::BeginTabItem("pad")) {
+            view_pad("pad", ctx.composition);
+            ImGui::EndTabItem();
+        }
+
+        ImGui::EndTabBar();
+    }
+    ImGui::NewLine();
+    ImGui::NewLine();
+
+    const bool should_exit = ImGui::Button("Bye!");
+
+    ImGui::Text("Framerate: %.2f", ImGui::GetIO().Framerate);
+    ImGui::End();
+
+    return !should_exit;
+};
+
+static void glfw_error_callback(int error, const char* description) {
+    fprintf(stderr, "GLFW Error %d: %s\n", error, description);
+}
+
+// Main code
+void GUI(context& ctx) {
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit())
+
+        // TODO - RETURN CODE: CAN'T RETURN 1, WHAT SHOULD I DO?
+        return;
+
+    // Decide GL+GLSL versions
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+    // GL ES 2.0 + GLSL 100 (WebGL 1.0)
+    const char* glsl_version = "#version 100";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+#elif defined(IMGUI_IMPL_OPENGL_ES3)
+    // GL ES 3.0 + GLSL 300 es (WebGL 2.0)
+    const char* glsl_version = "#version 300 es";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+#elif defined(__APPLE__)
+    // GL 3.2 + GLSL 150
+    const char* glsl_version = "#version 150";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
+#else
+    // GL 3.0 + GLSL 130
+    const char* glsl_version = "#version 130";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+#endif
+
+    // Create window with graphics context
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Dear ImGui GLFW+OpenGL3 example", nullptr, nullptr);
+    if (window == nullptr) {
+        // TODO - RETURN CODE: CAN'T RETURN 1, WHAT SHOULD I DO?
+        return;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);  // Enable vsync
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImPlot::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;   // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    // ImGui::StyleColorsLight();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    ctx.audio_ready.acquire();
+    // perform gui setup before audio starts
+    ctx.gui_ready.release();
+
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+    // Main loop
+    while (!glfwWindowShouldClose(window)) {
+        // Poll and handle events (inputs, window resize, etc.)
+        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to
+        // use your inputs.
+        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or
+        // clear/overwrite your copy of the mouse data.
+        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main
+        // application, or clear/overwrite your copy of the keyboard data. Generally you may always pass all
+        // inputs to dear imgui, and hide them from your application based on those two flags.
+        glfwPollEvents();
+        if (glfwGetWindowAttrib(window, GLFW_ICONIFIED) != 0) {
+            ImGui_ImplGlfw_Sleep(10);
+            continue;
+        }
+
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // Show window
+        if (!guiFunction(ctx)) {
+            glfwSetWindowShouldClose(window, true);
+        }
+
+        // Rendering
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(
+                clear_color.x * clear_color.w,
+                clear_color.y * clear_color.w,
+                clear_color.z * clear_color.w,
+                clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+    }
+
+    ctx.gui_intent_to_exit.release();
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImPlot::DestroyContext();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+}
