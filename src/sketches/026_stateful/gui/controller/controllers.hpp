a09f838aa4919785244471a8259b2e8ad107295e
Rob Albus <hello@robertalbus.com>
2025-06-01 18:48:54 -0600

    026_stateful create from 025_cymbal_ui

---
+++ src/sketches/026_stateful/gui/controller/controllers.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+// Sketches with Clover Audio Framework
+// Copyright (C) 2025  Rob W. Albus
+// Licensed under the GPLv3. See LICENSE for details.
+
+#include "lib/logging/logger.hpp"
+
+#include "graph/graph.hpp"
+
+struct tabbed_controller {
+    // tabbed_controller(tabbed_controller&&) = default;
+    tabbed_controller(const char* name) : name{name} {};
+    virtual ~tabbed_controller() = default;
+
+    const char* name;
+    virtual void draw(const char* id, signal_graph& graph, log_bus_000& logger) = 0;
+};
+
+struct controller_mixer : public tabbed_controller {
+    using tabbed_controller::tabbed_controller;
+    void draw(const char* id, signal_graph& graph, log_bus_000& logger) override;
+};
+
+struct controller_kick : public tabbed_controller {
+    using tabbed_controller::tabbed_controller;
+    void draw(const char* id, signal_graph& graph, log_bus_000& logger) override;
+};
+
+struct controller_ride : public tabbed_controller {
+    using tabbed_controller::tabbed_controller;
+    void draw(const char* id, signal_graph& graph, log_bus_000& logger) override;
+};
+
+struct controller_chord : public tabbed_controller {
+    using tabbed_controller::tabbed_controller;
+    void draw(const char* id, signal_graph& graph, log_bus_000& logger) override;
+};
