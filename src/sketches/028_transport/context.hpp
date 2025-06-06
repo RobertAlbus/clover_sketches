a1a4bd633ef46011c71591196254cc3821a052b9
Rob Albus <hello@robertalbus.com>
2025-06-05 18:46:49 -0600

    028_transport clone from 027_gpeq

---
+++ src/sketches/028_transport/context.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+// Sketches with Clover Audio Framework
+// Copyright (C) 2025  Rob W. Albus
+// Licensed under the GPLv3. See LICENSE for details.
+
+#include <semaphore>
+
+#include "clover/io/stream.hpp"
+
+#include "graph/graph.hpp"
+#include "gui/view.hpp"
+#include "lib/logging/logger.hpp"
+#include "sequence/sequencers.hpp"
+
+const float base_duration_bars = 32;
+struct context {
+    context(bool render_mode = false)
+        : duration_bars(render_mode ? base_duration_bars * 2.f : base_duration_bars) {
+        should_loop = !render_mode;
+    }
+    float fs  = 48000;
+    float bpm = 124;
+    float duration_bars;
+    bool should_loop = true;
+
+    int channel_count_out = 2;
+    const std::string render_name{"028_transport"};
+
+    log_bus_000 logger;
+
+    bar_grid grid{fs, bpm, duration_bars, should_loop};
+    signal_graph graph{grid};
+    sequencers sequencers{graph, grid, logger};
+    view view{graph, logger};
+
+    clover::io::stream* stream = nullptr;
+
+    std::binary_semaphore gui_ready{0};
+    std::binary_semaphore gui_intent_to_exit{0};
+
+    std::binary_semaphore audio_ready{0};
+};
---
+++ src/sketches/028_transport/gui/controller/controllers.cpp
@@ -0,0 +1,89 @@
+// Sketches with Clover Audio Framework
+// Copyright (C) 2025  Rob W. Albus
+// Licensed under the GPLv3. See LICENSE for details.
+
+#include <print>
+
+#include "graph/graph.hpp"
+#include "gui/components/cymbal_000.hpp"
+#include "gui/components/gpeq.hpp"
+#include "imgui.h"
+
+#include "lib/fdn/draw_fdn8_023.hpp"
+#include "lib/kick_drum/draw_kick_drum.hpp"
+#include "lib/logging/draw_gui_log_canvas.hpp"
+#include "lib/mixer/draw_mixer.hpp"
+#include "lib/peq/draw_peq.hpp"
+
+#include "controllers.hpp"
+
+void controller_mixer::draw(const char* id, signal_graph& graph, log_bus_000& logger) {
+    ImGui::PushID(name);
+    ImGui::PushID(id);
+    static log_canvas_000 canvas;
+    draw_gui_log_canvas_000("log_canvas", canvas, logger, nullptr);
+
+    static double x = 0.5f;
+    static double y = 0.5f;
+
+    static bool show_plot = true;
+    ImGui::Checkbox("show plot", &show_plot);
+    if (show_plot) {
+        master_peq.draw();
+    }
+
+    draw_mixer_000("new_mix", &graph.mixer_tracks);
+    draw_peq_000("##master_peq", graph.main_eq);
+
+    ImGui::PopID();
+    ImGui::PopID();
+}
+
+void controller_kick::draw(const char* id, signal_graph& graph, log_bus_000& logger) {
+    ImGui::PushID(name);
+    ImGui::PushID(id);
+
+    draw_kick_drum_000("kick_synth", graph.kick);
+    if (ImGui::BeginTable("##kick_table", 2)) {
+        ImGui::TableNextColumn();
+        draw_peq_000("##kick_preverb_peq", graph.kick_preverb_peq);
+        ImGui::TableNextColumn();
+        ImGui::Text("kick postverb peq");
+        draw_peq_000("##kick_out_peq", graph.kick_out_peq);
+        ImGui::EndTable();
+    }
+    draw_fdn8_023("##kick_fdn", &graph.kick_verb, nullptr);
+
+    ImGui::PopID();
+    ImGui::PopID();
+}
+
+void controller_ride::draw(const char* id, signal_graph& graph, log_bus_000& logger) {
+    ImGui::PushID(name);
+    ImGui::PushID(id);
+
+    draw_cymbal_000("ride", graph.ride);
+    draw_peq_000("##ride_eq", graph.ride_peq);
+
+    ImGui::PopID();
+    ImGui::PopID();
+}
+
+void controller_chord::draw(const char* id, signal_graph& graph, log_bus_000& logger) {
+    ImGui::PushID(name);
+    ImGui::PushID(id);
+    draw_fdn8_023("fdn", &graph.chord_verb_L, &graph.chord_verb_R);
+    if (ImGui::BeginTable("##peq_table", 2)) {
+        ImGui::TableNextColumn();
+        ImGui::Text("chord preverb peq");
+        draw_peq_000("##preverb_peq", graph.chord_preverb_peq);
+
+        ImGui::TableNextColumn();
+        ImGui::Text("chord out peq");
+        draw_peq_000("##postverb_peq", graph.chord_peq);
+
+        ImGui::EndTable();
+    }
+    ImGui::PopID();
+    ImGui::PopID();
+}
