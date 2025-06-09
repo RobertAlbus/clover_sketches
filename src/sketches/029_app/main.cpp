f98c76f414dcf79a5b3a951c4219f4ba91333d0c
Rob Albus <hello@robertalbus.com>
2025-06-08 19:31:56 -0600

    029_app clone from 028_transport

---
+++ src/sketches/029_app/main.cpp
@@ -0,0 +1,24 @@
+// Sketches with Clover Audio Framework
+// Copyright (C) 2025  Rob W. Albus
+// Licensed under the GPLv3. See LICENSE for details.
+
+#include <thread>
+
+#include "audio.hpp"
+#include "context.hpp"
+#include "gui.hpp"
+
+using namespace clover;
+using namespace io;
+
+int main(int, char *[]) {
+    context ctx;
+
+    std::thread Audio(AUDIO, std::ref(ctx));
+    std::thread Gui(GUI, std::ref(ctx));
+
+    Audio.join();
+    Gui.join();
+
+    return 0;
+}
