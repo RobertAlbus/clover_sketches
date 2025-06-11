592e74dfde9656cc49a6dadac277948e7681894b
Rob Albus <hello@robertalbus.com>
2025-06-10 23:16:02 -0600

    delete 025 026 027 028

--- src/sketches/026_stateful/gui.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// Sketches with Clover Audio Framework
-// Copyright (C) 2025  Rob W. Albus
-// Licensed under the GPLv3. See LICENSE for details.
-
-#include "context.hpp"
-
-void GUI(context& ctx);
