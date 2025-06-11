3f9e6d254b2fe2b436cc547213f454b7a2179b79
Rob Albus <hello@robertalbus.com>
2025-06-10 22:39:41 -0600

    lib: specify capacity and block recycling for log_bus_000

--- src/clover_sketches_lib/lib/logging/logger.hpp
+++ src/clover_sketches_lib/lib/logging/logger.hpp
@@ -6,10 +6,14 @@
 
 #include "concurrentqueue.h"
 
+struct custom_log_bus_traits_000 : moodycamel::ConcurrentQueueDefaultTraits {
+    static const bool RECYCLE_ALLOCATED_BLOCKS = true;
+};
+
 struct log_message_000 {
     char text[256];
 };
 
 struct log_bus_000 {
-    moodycamel::ConcurrentQueue<log_message_000> gui;
+    moodycamel::ConcurrentQueue<log_message_000, custom_log_bus_traits_000> gui{64 * sizeof(log_message_000)};
 };
