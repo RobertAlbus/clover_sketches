7f371a9ca0306f9f851a8f3fd7b057bec9e4d5f7
Rob Albus <hello@robertalbus.com>
2025-10-03 17:51:12 -0600

    lib: fix off-by-one error in env_bp_040::key_on_from(bar)

--- src/clover_sketches_lib/lib/env_bp/env_bp_040.cpp
+++ src/clover_sketches_lib/lib/env_bp/env_bp_040.cpp
@@ -39,9 +39,11 @@ void env_bp_040::key_on_from(double from_time_relative) {
     current_time_abs = (from_time_relative / props.duration_rel) * props.duration_abs;
     curr_point       = props.pattern.begin();
 
-    while (curr_point != props.pattern.end() && curr_point->start < from_time_relative) {
+    // Find the latest event that happens at or before from_time_relative
+    while (curr_point != props.pattern.end() && curr_point->start <= from_time_relative) {
         curr_point += 1;
     }
+    curr_point -= 1;
 }
 
 void env_bp_040::key_off() {
